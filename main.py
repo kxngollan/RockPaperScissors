9229046cb4c3ed7756339cdcafb5fd7bcac9d33f
Ollan Muza <128623143+kxngollan@users.noreply.github.com>
2023-12-11 03:34:31 +0000

    finished game

--- main.py
+++ main.py
@@ -28,3 +28,35 @@ scissors = '''
 import random
 
 selection = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+
+computer = random.randint(0, 2)
+if computer == 0:
+   computer = rock 
+elif computer == 1:
+   computer = paper
+else:
+   computer = scissors
+
+print(computer)
+
+if selection == 0:
+   selection = rock 
+elif selection == 1:
+   selection = paper
+else:
+   selection = scissors
+
+print(selection)
+
+print(f"You choose:\n {selection}")
+print(f"Computer choose:\n {computer}")
+if computer == selection:
+   print("Draw!")
+elif computer == rock and selection == paper:
+   print("You Win!")
+elif computer == paper and selection == scissors:
+   print("You Win!")
+elif computer == scissors and selection == rock:
+   print("You Win!")
+else:
+   print("You Lose!")
